20d183445509d4fefc8169f70b699d9f90f9ed31
gcosgreave <gcosgreave@me.com>
2017-02-02 21:36:59 +0000

    commit for courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,35 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/gcosgreave.json',
+    dataType: 'jsonp',
+    success: function(response){
+      addCourses(response.courses.completed);
+    }
+  });
+  function addCourses(courses){
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+      console.log('gc', course)
+      $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        text: 'See course'
+      }).appendTo($course);
+    })
+  }
 
 });
